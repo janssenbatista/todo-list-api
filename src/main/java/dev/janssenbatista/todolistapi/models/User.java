d5667ebbe3264d083c3a5c5e5484a0db1f2d110d
Janssen Batista <batistajanssen.dev@gmail.com>
2022-11-04 13:33:56 -0300

    updated user model

--- src/main/java/dev/janssenbatista/todolistapi/models/User.java
+++ src/main/java/dev/janssenbatista/todolistapi/models/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @Column(name = "first_name",length = 50, nullable = false)
+    @Column(name = "first_name", length = 50, nullable = false)
     private String firstName;
     @Column(name = "last_name", nullable = false)
     private String lastName;
