3fcca7e9f4084e47d754c1fee89bfed5d972eb3b
Janssen Batista <batistajanssen.dev@gmail.com>
2022-11-04 16:52:32 -0300

    created todo_list dto

---
+++ src/main/java/dev/janssenbatista/todolistapi/dtos/TodoListDto.java
@@ -0,0 +1,17 @@
+package dev.janssenbatista.todolistapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TodoListDto {
+    @NotBlank
+    private String description;
+}
